53a939a0a5ccf51615959ac3a356f5c3e06fb738
terryvdgriend <tgriend@student.avans.nl>
2015-09-08 09:43:02 +0200

    commit

---
+++ ALG2/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ALG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driehoek(100);
+            Console.WriteLine();
+            Fibonacci(15);
+            Console.WriteLine();
+            omgedraaidRec("Paul");
+            Console.WriteLine();
+            Console.WriteLine(omgedraaidPal("deelkoortsmeetsysteemstrookleed"));
+            Console.ReadKey();
+        }
+
+        static void Fibonacci(int n)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(fiboIte(n));
+            sw.Stop();
+            Console.WriteLine("iterative Elapsed={0}", sw.Elapsed);
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(fiboRec(n));
+            sw.Stop();
+            Console.WriteLine("recursive Elapsed={0}", sw.Elapsed);
+        }
+
+        static void omgedraaidRec(string n)
+        {
+            if (n.Length > 0)
+            {
+                Console.Write(n[n.Length - 1]);
+                n = n.Remove(n.Length - 1);
+                omgedraaidRec(n);
+            }
+
+        }
+
+        static bool omgedraaidPal(string n)
+        {
+            n = n.ToLower();
+            if (n.Length >= 2)
+            {
+                if (n[0] == n[n.Length - 1])
+                {
+                    n = n.Remove(0,1);
+                    n = n.Remove(n.Length - 1);
+                    return omgedraaidPal(n);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+
+
+        static int fiboRec(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return fiboRec(n - 1) + fiboRec(n - 2);
+        }
+
+        static int fiboIte(int n)
+        {
+            if(n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            int prev2 = 0;
+            int prev = 1;
+            int result = 0;
+            for (int i = 2; i <= n; i++)
+			{
+                result = prev + prev2;
+                prev2 = prev;
+                prev = result;
+			}
+            return result;
+        }
+
+        static void Driehoek(int n)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(iterative(n));
+            sw.Stop();
+            Console.WriteLine("iterative Elapsed={0}", sw.Elapsed);
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(recursive(n));
+            sw.Stop();
+            Console.WriteLine("recursive Elapsed={0}", sw.Elapsed);
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(functional(n));
+            sw.Stop();
+            Console.WriteLine("functional Elapsed={0}", sw.Elapsed);
+            Console.WriteLine();
+        }
+        static int iterative(int n)
+        {
+            int getal = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                getal = getal + i;
+            }
+            return getal;
+        }
+
+        static int recursive(int n)
+        {
+            if (n > 0)
+            {
+                n = n + recursive(n - 1);
+            }
+            
+            return n;
+        }
+        static int functional(int n)
+        {
+            n = (n / 2) * (n + 1);
+            return n;
+        }
+    }
+}
---
+++ sierpinski/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sierpinski
+{
+    public partial class Form1 : Form
+    {
+        int width = 0;
+        int height = 0;
+        private System.Drawing.Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            width = panel1.Size.Width;
+            height = panel1.Size.Height;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            int niveau = Convert.ToInt32(this.numericUpDown1.Value);
+            RectangleF rect = new RectangleF(0,0,width,height);
+            label3.Text = ""+Math.Pow(8, niveau);
+            DrawRectangle(g, niveau, rect);
+        }
+
+        // Draw a carpet in the rectangle.
+        private void DrawRectangle(Graphics gr, int niveau, RectangleF rect)
+        {
+            if (niveau == 0)
+            {
+                gr.FillRectangle(Brushes.Black, rect);
+            }
+            else
+            {
+                float wid = rect.Width / 3f;
+                float x0 = rect.Left;
+                float x1 = x0 + wid;
+                float x2 = x0 + wid * 2f;
+
+                float hgt = rect.Height / 3f;
+                float y0 = rect.Top;
+                float y1 = y0 + hgt;
+                float y2 = y0 + hgt * 2f;
+
+                DrawRectangle(gr, niveau - 1, new RectangleF(x0, y0, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x1, y0, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x2, y0, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x0, y1, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x2, y1, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x0, y2, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x1, y2, wid, hgt));
+                DrawRectangle(gr, niveau - 1, new RectangleF(x2, y2, wid, hgt));
+            }
+        }
+
+
+    }
+}
